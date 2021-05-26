0bcc417cbcde2de713f27cd6049d0e7e6ed039f0
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-05-26 12:23:55 -0400

    commit tree 59e22b2af895ecc3abd28069c41bf30908e607fd

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+
+import React from 'react';
+
+class App extends React.Component {
+
+  state = {
+    numer: 0,
+    message: "",
+    people: [{
+      name: "",
+      craft: ""
+    }]
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(data => this.setState({data}))
+  }
+
+  render() {
+    return(
+      <div>hi</div>
+    )
+  }
+}
+
+export default App 
